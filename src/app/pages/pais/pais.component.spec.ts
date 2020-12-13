9aa041b57c4f7ce17815716ca5b8810fd4e969f0
Wilson Martinez <wilsonmb89@hotmail.com>
2020-12-13 13:15:32 -0500

    update anbgular

--- src/app/pages/pais/pais.component.spec.ts
+++ src/app/pages/pais/pais.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PaisComponent } from './pais.component';
 
@@ -6,7 +6,7 @@ describe('PaisComponent', () => {
   let component: PaisComponent;
   let fixture: ComponentFixture<PaisComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PaisComponent ]
     })
--- src/app/pages/paises/paises.component.spec.ts
+++ src/app/pages/paises/paises.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PaisesComponent } from './paises.component';
 
@@ -6,7 +6,7 @@ describe('PaisesComponent', () => {
   let component: PaisesComponent;
   let fixture: ComponentFixture<PaisesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PaisesComponent ]
     })
