2bfe7a0eb9d950d9252c2f7a98785fd91ccb61a4
Wilson Martinez <wilsonmb89@hotmail.com>
2020-12-13 13:08:38 -0500

    a

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { PaisComponent } from './pages/pais/pais.component';
+import { PaisesComponent } from './pages/paises/paises.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: PaisesComponent },
+  { path: 'pais/:id', component: PaisComponent },
+  { path: '**', component: PaisesComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PaisComponent } from './pages/pais/pais.component';
+import { PaisesComponent } from './pages/paises/paises.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaisComponent,
+    PaisesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
